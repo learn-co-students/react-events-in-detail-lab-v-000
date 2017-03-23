d11abedad08943cb172445822ecbfa755f580d3e
Bruce Molina <bruce.molina.81@gmail.com>
2017-03-23 02:11:28 +0000

    Done.

--- components/CoordinatesButton.js
+++ components/CoordinatesButton.js
@@ -4,27 +4,22 @@ class CoordinatesButton extends React.Component {
   constructor(){
     super();
 
-    this.onReceiveCoordinates = this.onReceiveCoordinates.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  onReceiveCoordinates(event){
+  handleClick(event){
     const xTarget = event.pageX;
     const yTarget = event.pageY;
 
-    console.log([xTarget, yTarget])
+    this.props.onReceiveCoordinates([xTarget, yTarget])
   }
 
   render(){
     return (
-       <button onClick={this.onReceiveCoordinates}>Get The Mouse Coords! </button>
+       <button onClick={this.handleClick}>Get The Mouse Coords!</button>
     );
   }
 
 }
-
-
-
-
-
 
 module.exports = CoordinatesButton;
