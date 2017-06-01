42cbcb43c9d723d792795f1700250b293d4b1f81
Keegan Leitz <kjleitz@gmail.com>
2017-06-01 14:04:06 -0400

    Done.

---
+++ components/DelayedButton.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default class DelayedButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    event.persist();
+    setTimeout(() => {
+      this.props.onDelayedClick(event);
+    }, this.props.delay);
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick} >get mouse coords</button>
+    );
+  }
+}
