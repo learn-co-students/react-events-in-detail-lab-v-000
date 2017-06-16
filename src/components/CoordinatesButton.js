12cb2831e8eeaea74fa5e7ea8e7533e4549f443b
BDN <bennorris07@gmail.com>
2017-06-16 11:40:51 -0400

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,19 @@
-// Code CoordinatesButton Component Here
+import React from 'react';
+
+export default class CoordinatesButton extends React.Component {
+
+  trigger = (e) => {
+    const x = e.screenX;
+    const y = e.screenY;
+    const coords = [x,y]
+    this.props.onReceiveCoordinates(coords);
+  }
+
+
+  render() {
+    return (
+      <button onClick={this.trigger}>herman melville</button>
+    )
+  }
+
+}
