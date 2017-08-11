2406517fea33d800e93796a32a0360f449c275b4
mcclains2003 <mcclains2003@gmail.com>
2017-08-11 08:20:33 -0500

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,48 @@
-// Code CoordinatesButton Component Here
+import React from 'react';
+
+export default class CoordinatesButton extends React.Component {
+  constructor() {
+    super();
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    this.props.onReceiveCoordinates([event.clientX, event.clientY])
+  }
+
+  render() {
+    return(
+      <div>
+        <button onClick={this.handleClick}>
+          Click Me!
+        </button>
+      </div>
+    ) 
+  }
+}
+
+
+
+
+// import React from 'react';
+
+// export default class Keypad extends React.Component {
+//   constructor() {
+//     super();
+
+//     this.passcode = this.passcode.bind(this);
+//   }
+
+//   passcode() {
+//     console.log('Entering password...');
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <input type="password" onKeyUp={this.passcode} />
+//       </div>
+//     )
+//   }
+// }
