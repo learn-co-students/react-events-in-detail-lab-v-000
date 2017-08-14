4cd3fc3043c63872b618299a34937101be042099
mcclains2003 <mcclains2003@gmail.com>
2017-08-14 08:44:16 -0500

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -21,28 +21,3 @@ export default class CoordinatesButton extends React.Component {
     ) 
   }
 }
-
-
-
-
-// import React from 'react';
-
-// export default class Keypad extends React.Component {
-//   constructor() {
-//     super();
-
-//     this.passcode = this.passcode.bind(this);
-//   }
-
-//   passcode() {
-//     console.log('Entering password...');
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <input type="password" onKeyUp={this.passcode} />
-//       </div>
-//     )
-//   }
-// }
