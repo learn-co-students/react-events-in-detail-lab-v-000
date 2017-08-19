afb699d594e2e8517fc2f6d8ee09bab237861dad
github username <github email address>
2017-08-19 15:13:24 -0500

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,20 @@
-// Code CoordinatesButton Component Here
+import React from 'react';
+
+export default class CoordinatesButton extends React.Component {
+  constructor () {
+    super()
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(event) {
+    this.props.onReceiveCoordinates([event.screenX, event.screenY])
+  }
+
+  render () {
+    return (
+      <button onClick={this.handleClick}>Click me!</button>
+    )
+  }
+}
+
+// For reference: https://medium.com/@rjun07a/binding-callbacks-in-react-components-9133c0b396c6 
