a43006f6ab38efa6eb1bab52c554307978581a18
Howard DeVennish <howarddevennish@gmail.com>
2017-08-30 22:59:31 -0400

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,23 @@
-// Code CoordinatesButton Component Here
+import React from 'react';
+
+export default class CoordinateButton extends React.Component {
+  constructor() {
+    super();
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = (e) => {
+    this.props.onReceiveCoordinates([e.clientX, e.clientY])
+  }
+  render() {
+    return (
+      <button onClick={this.handleClick} >Coordinates Button</button>
+    );
+  }
+}
+
+// COORDINATESBUTTON
+// In the components/CoordinatesButton.js file, create a CoordinatesButton React component.
+// This component takes in one prop: onReceiveCoordinates. This prop is a function. (This props is only passed in the test suite, you will have errors in the browser)
+// When the button is clicked, create an array with two elements: the X and Y coordinates of the mouse. Find these using the event data.
+// The onReceiveCoordinates callback prop is then called with these coordinates.
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,26 @@
-// Code DelayedButton Component Here
+import React from 'react';
+
+export default class DelayedButton extends React.Component {
+  constructor() {
+    super();
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick = (e) => {
+    e.persist();
+    setTimeout(()=>{
+      this.props.onDelayedClick(e)
+    }, this.props.delay)
+  }
+  render() {
+    return (
+      <button onClick={this.handleClick} >Delayed Button</button>
+    );
+  }
+}
+
+// setTimeout(function(){ alert("Hello"); }, 3000);
+
+// DELAYEDBUTTON
+// In the components/DelayedButton.js file, create a DelayedButton React component
+// This component takes two props: onDelayedClick (a function), and delay (a number). (These props are only passed in the test suite, you will have errors in the browser)
+// When the button is clicked, we want to persist the event so we can pass it to the this.props.onDelayedClick() in a setTimeout() block. The setTimeout() will be set to this.props.delay.
