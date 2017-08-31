7d16372f5dfb7128276be663f8a9d1f8ca2ac5a4
Howard DeVennish <howarddevennish@gmail.com>
2017-08-31 10:46:00 -0400

    Done.

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -7,7 +7,7 @@ export default class DelayedButton extends React.Component {
   }
   handleClick = (e) => {
     e.persist();
-    setTimeout(()=>{
+    setTimeout(() => {
       this.props.onDelayedClick(e)
     }, this.props.delay)
   }
@@ -17,10 +17,3 @@ export default class DelayedButton extends React.Component {
     );
   }
 }
-
-// setTimeout(function(){ alert("Hello"); }, 3000);
-
-// DELAYEDBUTTON
-// In the components/DelayedButton.js file, create a DelayedButton React component
-// This component takes two props: onDelayedClick (a function), and delay (a number). (These props are only passed in the test suite, you will have errors in the browser)
-// When the button is clicked, we want to persist the event so we can pass it to the this.props.onDelayedClick() in a setTimeout() block. The setTimeout() will be set to this.props.delay.
