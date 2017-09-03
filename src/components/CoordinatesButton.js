c1a8d3d20bcb2823f9423a088a2e7cafe834456c
sarjumulmi <mulmi.sarju@gmail.com>
2017-09-03 00:15:10 -0400

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,15 @@
 // Code CoordinatesButton Component Here
+import React from 'react';
+
+export default class CoordinatesButton extends React.Component {
+
+  clickHandler = (e)=>{
+    this.props.onReceiveCoordinates([e.clientX, e.clientY])
+  }
+  render () {
+    return (
+      <button onClick={this.clickHandler}>Get Coordinate</button>
+    );
+
+  }
+}
