a490d337717734d7a3b0670a5234a78370d18540
Sophia Hubeny <shhubeny@gmail.com>
2017-12-17 01:45:22 +0000

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,17 @@
 // Code CoordinatesButton Component Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class CoordinatesButton extends React.Component {
+  handleClick = (event) => {
+      this.props.onReceiveCoordinates([event.clientX, event.clientY]) //calls the prop function listed below, and is passing coordinates to it
+  }
+  render() {
+    return(
+    <button onClick={this.handleClick}> Click! </button>
+  )}
+}
+
+CoordinatesButton.propTypes = {
+  onReceiveCoordinates : PropTypes.func
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,22 @@
 // Code DelayedButton Component Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class DelayedButton extends React.Component {
+  handleClick = (event) => { //pass in event argument
+    event.persist() //event needs to be persisted to avoid event pooling
+    setTimeout (() => { //setTimeout function, so this can access props
+      this.props.onDelayedClick(event), this.props.delay
+    })
+  }
+  render(){
+    return(
+      <button onClick = {this.handleClick}> Delay! </button>
+    )
+  }
+}
+
+DelayedButton.propTypes = {
+  delay : PropTypes.number,
+  onDelayedClick : PropTypes.func
+}
