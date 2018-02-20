eef83825dfe1f1307f05dba80ad629cdf0366eba
github username <github email address>
2018-02-19 22:36:57 -0600

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,19 @@
-// Code CoordinatesButton Component Here
+import React from 'react';
+
+class CoordinateButton extends React.Component {
+  createCoordinates = (event) => {
+    let arr = [event.clientX, event.clientY]
+    this.props.onReceiveCoordinates(arr)
+  }
+
+  render() {
+    return(
+      <div>
+        <button onClick={this.createCoordinates}>Coordinate Button</button>
+      </div>
+    )
+  }
+}
+
+export default CoordinateButton
+//this.props.onRecieveCoordinates should be in here, right?
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,17 @@
-// Code DelayedButton Component Here
+import React from 'react';
+
+class DelayedButton extends React.Component{
+  timedClick = (event) => {
+    event.persist()
+    setTimeout(() => {this.props.onDelayedClick(event)}, this.props.delay)
+  }
+  //setTimeout--whatever onDelayedClick does, it needs access to event
+  //this.props.delay is the number of seconds before this function fires
+  render() {
+    return(
+      <button onClick={this.timedClick}>Delayed Button</button>
+    )
+  }
+}
+
+export default DelayedButton
