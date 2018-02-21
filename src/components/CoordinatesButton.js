a63850c66d1622b522116dd9b497d72d32c42824
EC2 Default User <ec2-user@ip-10-0-0-95.ec2.internal>
2018-02-21 19:29:04 +0000

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,15 @@
 // Code CoordinatesButton Component Here
+
+import React from 'react';
+
+class CoordinatesButton extends React.Component {
+    loc = (event) => {
+        this.props.onReceiveCoordinates([event.clientX, event.clientY]);
+    }
+    
+    render() {
+            return ( <button onClick={this.loc}>Coordinates</button> );
+    }
+}
+
+export default CoordinatesButton
