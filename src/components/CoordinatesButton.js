117ceaf06de18aa55600ea4ed43aacd46f93807f
ackerm44 <ackerm44@gmail.com>
2018-02-28 15:39:24 -0500

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,21 @@
-// Code CoordinatesButton Component Here
+import React from 'react';
+
+export default class CoordinatesButton extends React.Component {
+  click = (e) => {
+    this.props.onReceiveCoordinates([e.pageX, e.pageY])
+  }
+
+  render() {
+    return (
+      <button onClick={this.click}>Clickable</button>
+    )
+  }
+}
+
+
+
+
+// 1. In the `components/CoordinatesButton.js` file, create a `CoordinatesButton` React component.
+// 2. This component takes in one prop: `onReceiveCoordinates`. This prop is a _function_. (This props is only passed in the test suite, you will have errors in the browser)
+// 3. When the button is clicked, create an array with two elements: the X and Y coordinates of the mouse. Find these using the event data.
+// 4. The `onReceiveCoordinates` callback prop is then called with these coordinates.
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,23 @@
-// Code DelayedButton Component Here
+import React from 'react';
+
+export default class DelayedButton extends React.Component {
+
+  handleDelay = (e) => {
+    e.persist()
+    setTimeout(() => {
+      this.props.onDelayedClick(e)
+    }, this.props.delay)
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleDelay}>Delay</button>
+    )
+  }
+}
+
+
+
+// 1. In the `components/DelayedButton.js` file, create a `DelayedButton` React component
+// 2. This component takes two props: `onDelayedClick` (a function), and `delay` (a number). (These props are only passed in the test suite, you will have errors in the browser)
+// 3. When the button is clicked, we want to persist the event so we can pass it to the `this.props.onDelayedClick()` in a `setTimeout()` block. The `setTimeout()` will be set to `this.props.delay`.
