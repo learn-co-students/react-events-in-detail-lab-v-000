49a7f865a7cf5a4d48795b7f7fef37ad2e1edc3e
Alexander Urizar <urizaraj@gmail.com>
2018-04-13 14:42:57 -0400

    Complete lab

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,17 @@
-// Code CoordinatesButton Component Here
+import React, { Component } from 'react';
+
+class CoordinatesButton extends Component {
+  parse = event => {
+    const coor = [event.pageX, event.pageY]
+    // console.log(coor)
+    this.props.onReceiveCoordinates(coor)
+  }
+
+  render() {
+    return (
+      <button onClick={this.parse} >This is the first button</button>
+    );
+  }
+}
+
+export default CoordinatesButton;
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,20 @@
-// Code DelayedButton Component Here
+import React, { Component } from 'react';
+
+class DelayedButton extends Component {
+  render() {
+    return (
+      <div>
+        <button
+          onClick={event => this.parse(event)}
+        ></button>
+      </div>
+    );
+  }
+
+  parse(event) {
+    event.persist()
+    setTimeout(() => this.props.onDelayedClick(event), this.props.delay)
+  }
+}
+
+export default DelayedButton;
