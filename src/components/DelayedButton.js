a861548dfd17f36298116ef24679c0f5a9f25656
Adjoa <adjoa.darien@gmail.com>
2018-05-21 14:00:51 +0000

    Done.

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,19 @@
 // Code DelayedButton Component Here
+import React from 'react';
+
+class DelayedButton extends React.Component {
+  handleClick = (e) => {
+    e.persist()
+    // setTimeout(() => {
+    //   this.props.onDelayedClick(e);
+    // }, this.props.delay)
+    
+    setTimeout(this.props.onDelayedClick, this.props.delay, e)
+  }
+    
+  render() {
+    return (<button onClick={this.handleClick} delay={this.props.delay}>Delay!</button>);
+  }
+}
+
+export default DelayedButton;
