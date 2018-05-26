3a80701a5d57d5d5d207af4e6116c7663d136733
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-05-26 16:49:08 -0400

    Done.

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,31 @@
 // Code DelayedButton Component Here
+
+import React from 'react'
+
+class DelayedButton extends React.Component {
+  handleClick = (event) => {
+    event.persist();
+    setTimeout(() => {
+      this.props.onDelayedClick(event);
+    }, this.props.delay);
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>Delayed</button>
+    );
+  }
+}
+
+// What they are asking for in this lab makes no sense to me whatsoever and
+// is totally out of context.
+// class DelayedButton extends React.Component {
+//   render() {
+//     return(
+//       <button onClick={this.props.onDelayedClick} {this.props.delay}/>
+//     )
+//   }
+//
+// }
+
+export default DelayedButton
