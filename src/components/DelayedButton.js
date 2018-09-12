fdf7debf56c4c6440648266ff8163e0a5a520cd8
Deborah Seow <deborahseow9@gmail.com>
2018-09-12 15:50:56 -0500

    Done.

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,18 @@
 // Code DelayedButton Component Here
+import React, {Component} from 'react';
+
+export default class DelayedButton extends Component {
+
+  handleClick = (event) => {
+    event.persist()
+    setTimeout(() => {this.props.onDelayedClick(event)}, this.props.delay
+    )
+  }
+//setTimeout is a javascript function that takes in a function and a time for delay - 2 arguments. You need the fat arrow funciton so it doesn't get immediately called!
+
+  render() {
+    return (
+      <button type="button" onClick={this.handleClick}>Delay Button</button>
+    )
+  }
+}
