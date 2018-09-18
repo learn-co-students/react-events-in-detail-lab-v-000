26a2977a7e8af6253b35baf2750e5e34f2ef1c8a
github username <github email address>
2018-09-18 09:54:35 -0400

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,24 @@
 // Code CoordinatesButton Component Here
+import React, { Component } from 'react';
+
+export default class CoordinatesButton extends Component {
+
+  // This component takes in one prop: onReceiveCoordinates. This prop is a function passed down
+  // from index.js. This function is currently just logging whatever is passed into it.
+  // Within CoordinatesButton, render a button. On click of the button, create an array with
+  // two elements: the X and Y coordinates of the mouse. Find these using the event data.
+  // Pass this event data in as the argument for the onReceiveCoordinates prop.
+  // If successful, the current x,y position of your mouse should be logged.
+
+  handleClick = (event) => this.props.onReceiveCoordinates([event.clientX, event.clientY])
+
+  render() {
+    return (
+      <div>
+        <button
+          onClick= {this.handleClick}>
+        </button>
+      </div>
+    )
+  }
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,26 @@
 // Code DelayedButton Component Here
+import React, { Component } from 'react';
+
+export default class DelayedButton extends Component {
+
+  // This component takes two props: onDelayedClick (a function), and delay (a number).
+  // Create a button that, when clicked, will pass the click event to the onDelayedClick prop
+  // within a setTimeout(). The setTimeout() should be set to this.props.delay.
+  // If successful, the event will be logged to the console once the timeout has finished.
+
+  handleClick = (event) => {
+    event.persist() // deal with pooling
+    //takes args of a fn and an amount of time
+    setTimeout(() => this.props.onDelayedClick(event), this.props.delay)
+  }
+
+  render() {
+    return (
+      <div>
+        <button
+          onClick = {this.handleClick} >
+        </button>
+      </div>
+    )
+  }
+}
