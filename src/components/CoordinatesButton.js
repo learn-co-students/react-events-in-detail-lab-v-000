5107bf25eea3f44d9a181c1db3bf716e8adeedd6
Gingertonic <bethmschofield@gmail.com>
2018-09-18 11:45:52 +1000

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,16 @@
 // Code CoordinatesButton Component Here
+import React, { Component } from 'react';
+
+export default class CoordinatesButton extends Component {
+
+  makeArray = (event) => {
+    const coords = [event.clientX, event.clientY];
+    this.props.onReceiveCoordinates(coords);
+  }
+
+  render(){
+    return(
+      <button onClick={this.makeArray}>Button McButtonFace</button>
+    )
+  }
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,15 @@
 // Code DelayedButton Component Here
+import React, { Component } from 'react';
+
+export default class DelayedButton extends Component {
+  startWait = (event) => {
+    event.persist();
+    setTimeout(() => this.props.onDelayedClick(event), this.props.delay)
+  }
+
+  render(){
+    return(
+      <button onClick={this.startWait}>Delayed Button</button>
+    )
+  }
+}
