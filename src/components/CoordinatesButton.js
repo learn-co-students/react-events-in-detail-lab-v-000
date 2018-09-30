d841d15523fab4eb7bc9fa9ca9d07cd36d4501f9
Razelle McCarrick <razelle@satyaorthopedic.com>
2018-09-30 16:50:49 -0400

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,14 @@
 // Code CoordinatesButton Component Here
+import React, {Component} from 'react';
+
+export default class CoordinatesButton extends Component {
+
+  handleClick = (e) => {
+    const coordinates = [e.clientX, e.clientY]
+    this.props.onReceiveCoordinates(coordinates)
+  }
+
+  render (){
+    return <button onClick={this.handleClick}></button>
+  }
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,16 @@
 // Code DelayedButton Component Here
+import React, {Component} from 'react';
+
+
+export default class DelayedButton extends Component {
+  
+  handleClick = (e) => {
+    e.persist()
+    const timeOutFunction = this.props.onDelayedClick(e)
+    setTimeout(function(){timeOutFunction}, this.props.delay)
+  }
+
+  render (){
+    return <button onClick={this.handleClick}></button>
+  }
+}
