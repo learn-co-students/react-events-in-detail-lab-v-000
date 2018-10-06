fd39dafa9691a13fdbef4dbec8c24e55eedd0b6d
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2018-10-06 17:57:32 -0400

    Implement CoordinatesButton

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,8 @@
 // Code DelayedButton Component Here
+import React, { Component } from 'react'
+
+export default class DelayedButton extends Component {
+    render() {
+        return <button>Delayed Button</button>
+    }
+}
