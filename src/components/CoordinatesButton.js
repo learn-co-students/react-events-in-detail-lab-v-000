d1c3dd1aa9ea23205097b28022a9e9bd2a253e86
BarbaraPruz <barbara.prusiewicz@gmail.com>
2018-10-09 22:34:04 -0700

    Implement buttons.  Test for pooled event failing

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,25 @@
 // Code CoordinatesButton Component Here
+import React, { Component } from 'react';
+
+export default class CoordinatesButton extends Component {
+  
+    constructor (props) {
+        super(props);
+        this.state = {
+            onReceiveCoordinates: props.onReceiveCoordinates
+        };
+    }
+
+    handleClick = (e) => {
+        (this.props.onReceiveCoordinates)([e.clientX, e.clientY]);
+    }  
+
+    render() {
+        return (
+            <div>
+                <button type="button" onClick={this.handleClick}>Coordinates</button>
+            </div>
+        )
+    }
+  
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,21 @@
 // Code DelayedButton Component Here
+import React, { Component } from 'react';
+
+export default class DelayedButton extends Component {
+
+    handleClick = (e) => {
+        const event = e;
+        setTimeout(this.props.onDelayedClick.bind(null,event),this.props.delay)
+    }
+
+    render() {
+        return (
+            <div>
+                <button type="button" onClick={this.handleClick}>
+                    Delayed
+                </button>
+            </div>
+        )
+    }
+  
+}
