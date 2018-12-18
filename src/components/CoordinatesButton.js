f820b5a400b3f6ea9730bc32d571ea28990eeb37
terryblue99 <terryblue@comcast.net>
2018-12-18 17:00:02 -0600

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,5 +1,12 @@
 import React from 'react'
 
+// This component takes in one prop: onReceiveCoordinates. This prop is a function 
+// passed down from index.js. This function is currently just logging whatever is passed into it.
+// Render a button. On click of the button, create an array with two elements: the X and Y 
+// coordinates of the mouse. Find these using the event data.
+// Pass this event data in as the argument for the onReceiveCoordinates prop.
+// If successful, the current x,y position of the mouse should be logged.
+
 export default class CoordinatesButton extends React.Component {
 	handleClick = (event) => {
 	  this.props.onReceiveCoordinates([event.clientX, event.clientY]);
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,5 +1,10 @@
 import React from 'react'
 
+// This component takes two props: onDelayedClick (a function), and delay(a number).
+// Create a button that, when clicked, will pass the click event to the onDelayedClick prop 
+// within a setTimeout(). The setTimeout() should be set to this.props.delay.
+// If successful, the event will be logged to the console once the timeout has finished.
+
 export default class DelayedButton extends React.Component {
 	handleClick = (event) => {
 		event.persist()
