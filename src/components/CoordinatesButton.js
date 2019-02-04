1dc06f4193e901d5e4d9383d92854c81b0d81749
julianjohannesen <github email address>
2019-02-04 12:05:39 -0500

    initial

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,21 @@
 // Code CoordinatesButton Component Here
+import React, { Component } from 'react'
+
+export default class CoordinatesButton extends Component {
+
+    oRC = this.props.onReceiveCoordinates;
+
+    getXY = (event) => {
+        const theArray = [event.clientX, event.clientY];
+        console.log("Inside getXY: ", theArray);
+        return theArray; 
+    }
+
+    
+    render() {
+        
+        return (
+            <button onClick={(e)=>{this.oRC(this.getXY(e))}}>Button</button>
+            )
+    }
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,15 @@
 // Code DelayedButton Component Here
+import React, { Component } from 'react'
+
+export default class DelayedButton extends Component {
+
+    handler = (e) => {
+        window.setTimeout(this.props.onDelayedClick, this.props.delay);
+    }
+
+    render() {
+        return (
+            <button onClick={this.handler}>DelayedButton</button>
+        )
+    }
+}
