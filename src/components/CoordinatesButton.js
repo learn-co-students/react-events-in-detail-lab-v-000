8cf4d5642f322eabad26ccaba8b07dcd09219fb3
netzay <netzelally@gmail.com>
2019-02-05 15:31:11 -0700

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,15 @@
 // Code CoordinatesButton Component Here
+import React, {Component} from 'react';
+
+export default class CoordinatesButton extends Component {
+
+  createArray = (event) => {
+    this.props.onReceiveCoordinates([event.clientX, event.clientY])
+  }
+//We're not ASSIGNING above, but callin ga function from the parent
+  render() {
+    return (
+      <button type="button" onClick={this.createArray}>Coordinates Button</button>
+    )
+  }
+}
