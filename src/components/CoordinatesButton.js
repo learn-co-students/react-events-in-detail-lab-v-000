457612e024dbd688492006cedeff6c9e5015b55e
amitnyc83 <amit_nyc@live.com>
2019-04-01 14:50:32 -0400

    all test passing

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,17 @@
 // Code CoordinatesButton Component Here
+import React from 'react';
+
+// create a CoordinatesButton React component
+export default class CoordinatesButton extends React.Component {
+
+  currentCoordinates = (event) => {
+    let coordinates = [event.clientX, event.clientY]
+    this.props.onReceiveCoordinates(coordinates)//function passed down from index.js
+  }
+//renders a button; on click, creates an array with 2 elements. event data uses client X & Y to find coordinates of click
+  render(){
+    return (
+      <button onClick={this.currentCoordinates}>Click me!</button>
+    )
+  }
+}
