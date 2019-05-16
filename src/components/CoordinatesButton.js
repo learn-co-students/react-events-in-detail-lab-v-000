ce442998d212d4f4d97b0dd53c8d7f87f18abe6a
Christopher P. Dundon <chris.dundon@gmail.com>
2019-05-16 14:48:49 -0400

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,24 @@
-// Code CoordinatesButton Component Here
+import React, { Component } from 'react';
+
+export default class CoordinatesButton extends Component {
+  
+  constructor(props) {
+    super(props)
+		this.state = {
+      bubbleCoordinates: this.props.onReceiveCoordinates
+    }
+  }
+
+  handleClick = (e) => {const x_y = [e.clientX, e.clientY]
+											this.state.bubbleCoordinates(x_y)
+										}
+
+  render() {
+    return (
+      <div className="no-delay">
+				<button onClick={e => this.handleClick(e)}>Coordinates Button</button>
+      </div>
+    )
+  }
+  
+}
