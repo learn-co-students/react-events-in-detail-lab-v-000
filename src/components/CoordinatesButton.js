18e27530335cfeda89ccade48699dcccf326c4cd
Guess <guess@Guesss-iMac.local>
2019-06-21 14:22:23 -0400

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,18 @@
 // Code CoordinatesButton Component Here
+import React, { Component } from 'react'
+
+class DelayedButton extends Component{
+
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+
+        return (
+            <button onClick={(e) => this.props.onReceiveCoordinates([e.clientX, e.clientY])}>Coordinate Button</button>
+        )
+    }
+}
+
+export default DelayedButton
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,15 @@
 // Code DelayedButton Component Here
+import React, { Component } from 'react'
+
+export default class CoordinatesButton extends Component{
+
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+
+        return <button onClick={(e) => setTimeout(() => {e.persist(); this.props.onDelayedClick(e, this.props.delay)})}>Delayed Button</button>
+    }
+
+}
