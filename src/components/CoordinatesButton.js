9401d7177ed191cc542843008362b800433016af
Ashley <ashleywong678@gmail.com>
2019-07-31 17:18:35 -0400

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,18 @@
-// Code CoordinatesButton Component Here
+import React from 'react';
+
+class CoordinatesButton extends React.Component{
+
+  constructor(prop) {
+    super(prop)
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return(
+      <div></div>
+    )
+  }
+}
+export default CoordinatesButton;
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,11 @@
-// Code DelayedButton Component Here
+import React from 'react';
+
+class DelayedButton extends React.Component{
+
+  render() {
+    return(
+      <div></div>
+    )
+  }
+}
+export default DelayedButton;
