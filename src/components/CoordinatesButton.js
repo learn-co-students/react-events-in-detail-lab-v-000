8beb2f53c1dda5422fc13a65c1354a6e2ae2e8be
Pamela Frieze Torres-Rocca <pamela@Morpheus.local>
2019-09-17 20:37:13 -0400

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,20 @@
 // Code CoordinatesButton Component Here
+import React, { Component } from 'react';
+import DelayedButton from './DelayedButton';
+
+
+export default class CoordinatesButton extends Component {
+
+
+handleClick = (event) => {
+	event.persist();
+	const mouseCoordinates = [];
+	mouseCoordinates.push(event.clientX, event.clientY);  //event pooling 
+}
+
+render() {
+    return (
+    	<button onClick={this.props.onReceiveCoordinates(mouseCoordinates)}></button>
+    	)
+    }
+}
