cff294c685b3594c1b1afb7323a190ef885a546d
Pamela Frieze Torres-Rocca <pamela@Morpheus.local>
2019-09-18 17:15:48 -0400

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -4,20 +4,26 @@ import React, { Component } from 'react';
 
 const mouseCoordinates = [];
 
+//this.props.onReceivesCoordinates  receives prop 
 
 export default class CoordinatesButton extends Component {
 
 handleClick = (event) => {
 	mouseCoordinates.push(event.clientX, event.clientY);  
+	this.props.onReceiveCoordinates(mouseCoordinates)
+	console.log('hello')
+    console.log(mouseCoordinates)
 	}
 
-render(props, mouseCoordinates) {
+render(event, mouseCoordinates) {
     return (
     	<div>
     	<button onClick={this.handleClick}>
     	</button>
-    	<div>{this.props.onReceiveCoordinates(mouseCoordinates)}</div>
+    	<div>
+    	</div>
     	</div>
     	)
     }
+
 }
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -2,12 +2,21 @@
 import React, { Component } from 'react';
 //import CoordinatesButton from './CoordinatesButton';
 
+//This component takes two props: onDelayedClick (a function), and delay (a number).
 
 export default class DelayedButton extends Component {
 
+
+handleClick = event => {
+    event.persist();
+    setTimeout(() => {
+      this.props.onDelayedClick(event);
+    }, this.props.delay);
+  };
+
 	render() {
     return (
-    	<button></button>
+    	<button onClick={this.handleClick}></button>
 		)
     }
 }
