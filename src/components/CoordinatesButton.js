3af83ee58f3939f79ce73afe2b17dd71c84d1acc
github username <github email address>
2020-07-16 11:55:36 -0700

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,70 @@
 // Code CoordinatesButton Component Here
+// import React { Component } from 'react';
+import React from 'react';
+
+
+
+export default class  CoordinatesButton extends React.Component {
+
+
+
+  onReceiveCoordinates = (event) => {
+    // let array1 = [event.clientX, event.clientY]
+    // this.props.onReceiveCoordinates
+    console.log(event)
+  }
+
+
+  render() {
+    return(
+      <button onClick={ () => {this.onReceiveCoordinatesCallback} > hi </button>
+    )
+  }
+
+}
+
+
+ //
+ // export default class Clicker extends React.Component {
+ //
+ //   handleClick = (event) => {
+ //     console.log(event.type); // prints 'click'
+ //   }
+ //
+ //   render() {
+ //     return (
+ //       <button onClick={this.handleClick}>Click me!</button>
+ //     );
+ //   }
+ // }
+
+ // src/Matrix.js
+
+  
+//  constructor() {
+//    super()
+//    this.state = {
+//      selectedColor: '#FFF'
+//    }
+//  }
+//
+//
+// setSelectedColor = (newColor) => {
+//    this.setState({
+//      selectedColor: newColor
+//    })
+//  }
+
+ //
+ // export default class Clicker extends React.Component {
+ //
+ //   handleClick = (event) => {
+ //     console.log(event.type); // prints 'click'
+ //   }
+ //
+ //   render() {
+ //     return (
+ //       <button onClick={this.handleClick}>Click me!</button>
+ //     );
+ //   }
+ // }
