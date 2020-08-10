1fa58d1a672828ee7049bc40b31561589e3abaad
Sushi Chanrai <sushi@pathworks.co>
2020-08-10 12:54:19 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import ReactDOM from 'react-dom';
 import CoordinatesButton from './components/CoordinatesButton';
 import DelayedButton from './components/DelayedButton';
 
+
+
 ReactDOM.render(
   <div>
-    <CoordinatesButton onReceiveCoordinates={ mouseCoordinates => console.log(mouseCoordinates) } />
+    <CoordinatesButton onReceiveCoordinates={mouseCoordinates => console.log(mouseCoordinates) } />
     <DelayedButton onDelayedClick={ event => console.log(event) } delay={1500} />
+
   </div>,
   document.getElementById('global')
 );
