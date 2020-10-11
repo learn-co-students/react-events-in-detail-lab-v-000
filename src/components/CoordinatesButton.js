c3da96f31ff63bf61971eeb02e86521438bffe4d
RonSala <ron.sala@gmail.com>
2020-10-11 16:25:04 -0400

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
 
 class CoordinatesButton extends Component {
-  
-  constructor(props) {
-    super(props)
-  }
 
-  handleClick = (event) => {
-    ary = new Array(event.clientX, event.clientY)
+  handleClick = (event) => {    
+    this.props.onReceiveCoordinates([event.clientX, event.clientY]);
   }
 
   render() { 
